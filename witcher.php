2e711b1f81d2cff23b3d1682f0cce9653a581dcc
s19367 <pmatecki@edu.cdv.pl>
2015-07-18 15:19:56 +0200

    Initial commit

--- witcher.php
+++ witcher.php
@@ -1,10 +1,17 @@
 <?php
-require_once  'creature.php';
+include_once  'creature.php';
+
+/** klasa wiedzmin, dziedziczy z creature
+ * Class witcher
+ */
 class witcher extends creature
 {
     private $Ex;
     private $Ex_lvl;
     private $Ex_active;
+    private $defcap;
+    private $pass;
+    private $bonus;
 
     /**
      * @param $speed
@@ -18,88 +25,70 @@ class witcher extends creature
         parent::__construct($speed, $str, $agi, $live, $livemax);
     }
 
-    /**
+    /** ustawia bonus obrony
      * @return bool
      */
     public function def()
     {
-        $i=parent::getagi();
-        $i+=$i;
-        parent::setagi($i);
-        return true;
+        $this->defcap= true;
+        $bonus = parent::getagi();
+        $tp = $bonus/2;
+        $bonus += $tp;
+        $this->bonus= $bonus;
+        $this->pass=true;
+        return $bonus;
     }
 
-    /**
+    /** zwraca bonus za obrone
+     * @return mixed
+     */
+    public function getdef()
+    {
+        return $this->bonus;
+    }
+
+    /** sprawdza czy jest aktywny bonus obrony
+     * @return mixed
+     */
+    public function defcap()
+    {
+        return $this->defcap;
+    }
+
+    /** usuwa aktywny bonus obrony
+     *
+     */
+    public function del_defcap()
+    {
+        $this->pass=false;
+        $this->defcap=false;
+    }
+
+    /** sorwadza czy gracz spasowal
+     * @return mixed
+     */
+    public  function CheckPass()
+    {
+        return $this->pass;
+    }
+
+    /** usuwa aktywny pass
+     *
+     */
+    public function depass()
+    {
+        $this->pass=false;
+    }
+
+    /** ustawia pass na true, zwieksza ap++
      * @return int
      */
     public function pass()
     {
+        $this->pass=true;
         return parent::get_ap()+1;
     }
 
-    /**
-     * @param $i
-     */
-    public function crtEx($i)
-    {
-        switch(rand(1, 3))
-        {
-            case 1:
-                $this->Ex=1;
-                $this->Ex_lvl=$i;
-                break;
-            case 2:
-                $this->Ex=2;
-                $this->Ex_lvl=$i;
-                break;
-            case 3:
-                $this->Ex=3;
-                $this->Ex_lvl=$i;
-                break;
-            default:
-                echo'blad tworzenia eliksiru';
-                break;
-        }
-    }
-
-    /**
-     * @param $Ex
-     * @return int|null
-     */
-    public function drinkEx($Ex)
-    {
-        if( $this->Ex_active)
-        {
-            echo "Eliksir aktywny";
-            return null;
-        }
-        switch($Ex)
-        {
-            case 1:
-                $speed=parent::getspeed();
-                $speed==$this->Ex_lvl*10;
-                $this->Ex_active=true;
-                return $speed;
-                break;
-            case 2:
-                $str=parent::getstr();
-                $str=$this->Ex_lvl-10;
-                $this->Ex_active=true;
-                return $str;
-                break;
-            case 3:
-                $live=parent::getlivemax();
-                $sLive=$this->Ex_lvl*20;
-                $mLive=parent::getlive()+$sLive;
-                if ($mLive>$live)
-                    return $live;
-                else
-                    return $mLive;
-                break;
-            default:
-                echo 'Elixir error';
-        }
-    }
 
 
 
