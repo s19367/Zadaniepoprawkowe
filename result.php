eeff97aa2e235c8902818b397b49f052f44c3c9a
s19367 <pmatecki@edu.cdv.pl>
2015-07-18 13:13:22 +0200

    Initial commit

---
+++ result.php
@@ -0,0 +1,33 @@
+﻿<?php
+session_start();
+require_once 'witcher.php';
+include_once 'creature.php';
+include_once 'eliksir.php';
+$witcher = unserialize($_SESSION['witcher']);
+$monster = unserialize($_SESSION['monster']);
+$ap = $witcher->get_ap();
+switch($_POST['btn'])
+{
+    case 1:
+        if($ap-1 <= 0)
+        {
+            echo '<p>Brak AP!</p>';
+            break;
+        }
+        $rand = rand(0, 100);
+        $SK=$witcher->sk($witcher->getagi(), $monster->getagi());
+        if ($SK>=$rand)
+        {
+            $monster->setlive($monster->getlive() - $witcher->getstr());
+            echo '<p> sukces! </p>';
+        }
+        else
+            echo '<p> pudło! </p>';
+        $ap--;
+        break;
+    
+
+
+        echo '<p> action error </p>';
+}
+?>
